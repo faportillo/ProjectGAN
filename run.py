e54d1b01672a9d114234fe20a3be21be5552cf6a
Felix <faportillo@ucdavis.edu>
2020-05-26 22:16:24 -0700

    Reformatted loss functions to run.py instead of using losses.py (for now). Will expand losses.py to have new losses in class form

--- run.py
+++ run.py
@@ -109,7 +109,19 @@ if __name__ == '__main__':
     # Conventions for real and fake labels during training
     real_label = 1
     fake_label = 0
-
+    if loss_type == 'BCE':
+        criterion = nn.BCELoss()
+    elif loss_type == 'Hinge':
+        criterion = nn.HingeEmbeddingLoss()
+    elif loss_type == 'Wass':
+        fake_label = -1
+        pass  # ToDO: Implement Wasserstein Loss as class
+    elif loss_type == 'DCGAN':
+        pass  # ToDO: Implement DCGAN Loss as class
+    else:
+        raise ValueError('''Unsupported Loss Type!
+                            Supported losses for Discriminator are:
+                            \'BCE\', \'Wass\', \'Hinge\'''')
     # Setup optimizers for Generator and Discriminator
     optimizerG = optim.Adam(filter(lambda p: p.requires_grad,
                                    netG.parameters()), lr=g_lr, betas=(beta1, beta2))
@@ -138,25 +150,34 @@ if __name__ == '__main__':
                 # Format batch
                 real_cpu = data[0].to(device)
                 b_size = real_cpu.size(0)
+                label = torch.full((b_size,), real_label, device=device)
                 # Forward pass REAL batch through Discrim D(x)
                 d = netD(real_cpu).view(-1, 1)
+                # Calculate loss for REAL Batch
+                d_loss = criterion(d, label)
+
                 # Generate batch of latent vectors
                 noise = torch.randn(b_size, nz, 1, 1, device=device)
                 # Generate FAKE image batch with Gen
                 fake = netG(noise)
+                label.fill_(fake_label)
                 # Classify all fake batch with Discrim: D(G(z))
                 d_g = netD(fake.detach()).view(-1, 1)
                 ###test_d = d_g.cpu().detach().numpy() # Test print
                 ###print(test_d) # Test print
                 # Calculate losses for real and fake batches
-                dis_loss = loss_discriminator(d, d_g, loss_type=loss_type, batch_size=b_size)
+                dg_loss = criterion(d_g, label)
+                # dis_loss = loss_discriminator(d, d_g, loss_type=loss_type, batch_size=b_size)
                 # print("Discriminator Loss {}: {}".format(dis_i, dis_loss))
+
                 # Calculate gradients for Discrim
-                dis_loss.backward()
+                d_loss.backward()
+                dg_loss.backward()
+                dis_loss = d_loss + dg_loss
                 # Update D
                 optimizerD.step()
-                D_x = d.mean().item()
-                D_G_z1 = d_g.mean().item()
+                D_x = d.mean()
+                D_G_z1 = d_g.mean()
 
             ############################
             # (2) Update G network: maximize log(D(G(z)))
@@ -164,70 +185,73 @@ if __name__ == '__main__':
             netG.zero_grad()
             # Generate batch of latent vectors
             noise = torch.randn(batch_size, nz, 1, 1, device=device)
+            label = torch.full((batch_size,), real_label, device=device)
             fake = netG(noise)
             d_g = netD(fake).view(-1, 1)
             # Calculate Gen's loss based on output
-            gen_loss = loss_generator(d_g, loss_type=loss_type, batch_size=batch_size)
+            # gen_loss = loss_generator(d_g, loss_type=loss_type, batch_size=batch_size)
+            gen_loss = criterion(d_g, label)
             # Calcualte gradient for G
             gen_loss.backward()
             # Update G
             optimizerG.step()
-            D_G_z2 = d_g.mean().item()
+            D_G_z2 = d_g.mean()
 
             # Output training stats
             if i % 50 == 0:
                 print('[{}/{}][{}/{}]\tLoss_D: {}\tLoss_G: {}\tD(x): {}\tD(G(z)): {}/{}'
                       .format(epoch + 1, num_epochs, i, len(dataloader),
-                              dis_loss.item(), gen_loss.item(), D_x, D_G_z1, D_G_z2))
-            # Save Losses for plotting later
-            G_losses.append(dis_loss.item())
-            D_losses.append(gen_loss.item())
-            # Check how the generator is doing by saving G's output on fixed_noise
-            if (iters % 500 == 0) or ((epoch == num_epochs - 1) and (i == len(dataloader) - 1)):
-                with torch.no_grad():
-                    fake = netG(fixed_noise).detach().cpu()
-                img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+                              dis_loss.item(), gen_loss.item(),
+                              D_x.item(), D_G_z1.item(), D_G_z2.item()))
+                # Save Losses for plotting later
+                G_losses.append(dis_loss.item())
+                D_losses.append(gen_loss.item())
+                # Check how the generator is doing by saving G's output on fixed_noise
+                if (iters % 500 == 0) or ((epoch == num_epochs - 1) and (i == len(dataloader) - 1)):
+                    with torch.no_grad():
+                        fake = netG(fixed_noise).detach().cpu()
+                    img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
 
-            iters += 1
+                iters += 1
 
-    # Ssve models after training
-    torch.save(netD.state_dict(), 'saved_models/discriminator_' + model_type + '_' + loss_type + '.pt')
-    torch.save(netG.state_dict(), 'saved_models/generator' + model_type + '_' + loss_type + '.pt')
-    if args.dev is False:
-        plt.figure(figsize=(10, 5))
-        plt.title("Generator and Discriminator Loss During Training")
-        plt.plot(G_losses, label="G")
-        plt.plot(D_losses, label="D")
-        plt.xlabel("iterations")
-        plt.ylabel("Loss")
-        plt.legend()
-        # plt.show()
-        plt.savefig('GenDiscrimLoss.png', bbox_inches='tight')
+        # Ssve models after training
+        torch.save(netD.state_dict(), 'saved_models/discriminator_' + model_type + '_' + loss_type + '.pt')
+        torch.save(netG.state_dict(), 'saved_models/generator' + model_type + '_' + loss_type + '.pt')
+        if args.dev is False:
+            plt.figure(figsize=(10, 5))
+            plt.title("Generator and Discriminator Loss During Training")
+            plt.plot(G_losses, label="G")
+            plt.plot(D_losses, label="D")
+            plt.xlabel("iterations")
+            plt.ylabel("Loss")
+            plt.legend()
+            # plt.show()
+            plt.savefig('GenDiscrimLoss.png', bbox_inches='tight')
 
-        # %%capture
-        fig = plt.figure(figsize=(8, 8))
-        plt.axis("off")
-        ims = [[plt.imshow(np.transpose(i, (1, 2, 0)), animated=True)] for i in img_list]
-        ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
+            # %%capture
+            fig = plt.figure(figsize=(8, 8))
+            plt.axis("off")
+            ims = [[plt.imshow(np.transpose(i, (1, 2, 0)), animated=True)] for i in img_list]
+            ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
 
-        HTML(ani.to_jshtml())
+            HTML(ani.to_jshtml())
 
-        # Grab a batch of real images from the dataloader
-        real_batch = next(iter(dataloader))
+            # Grab a batch of real images from the dataloader
+            real_batch = next(iter(dataloader))
 
-        # Plot the real images
-        plt.figure(figsize=(15, 15))
-        plt.subplot(1, 2, 1)
-        plt.axis("off")
-        plt.title("Real Images")
-        plt.imshow(
-            np.transpose(vutils.make_grid(real_batch[0].to(device)[:64],
-                                          padding=5, normalize=True).cpu(), (1, 2, 0)))
-        plt.savefig("real_images.png", bbox_inches='tight')
-        # Plot the fake images from the last epoch
-        plt.subplot(1, 2, 2)
-        plt.axis("off")
-        plt.title("Fake Images")
-        plt.imshow(np.transpose(img_list[-1], (1, 2, 0)))
-        plt.show()
-        plt.savefig("fake_images.png", bbox_inches='tight')
+            # Plot the real images
+            plt.figure(figsize=(15, 15))
+            plt.subplot(1, 2, 1)
+            plt.axis("off")
+            plt.title("Real Images")
+            plt.imshow(
+                np.transpose(vutils.make_grid(real_batch[0].to(device)[:64],
+                                              padding=5, normalize=True).cpu(), (1, 2, 0)))
+            plt.savefig("real_images.png", bbox_inches='tight')
+            # Plot the fake images from the last epoch
+            plt.subplot(1, 2, 2)
+            plt.axis("off")
+            plt.title("Fake Images")
+            plt.imshow(np.transpose(img_list[-1], (1, 2, 0)))
+            plt.show()
+            plt.savefig("fake_images.png", bbox_inches='tight')
