9910c535760236c5fab8e0fe29d6965c5d721aeb
Felix <faportillo@ucdavis.edu>
2020-05-28 14:32:23 -0700

    applied dev bool to image display before training

--- train.py
+++ train.py
@@ -80,16 +80,16 @@ if __name__ == '__main__':
 
     # Decide which device we want to run on
     device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
-
-    # Plot some training images
-    real_batch = next(iter(dataloader))
-    plt.figure(figsize=(8, 8))
-    plt.axis("off")
-    plt.title("Training Images")
-    plt.imshow(
-        np.transpose(vutils.make_grid(real_batch[0].to(device)[:64],
-                                      padding=2, normalize=True).cpu(), (1, 2, 0)))
-    # plt.show()
+    if args.dev is False:
+        # Plot some training images
+        real_batch = next(iter(dataloader))
+        plt.figure(figsize=(8, 8))
+        plt.axis("off")
+        plt.title("Training Images")
+        plt.imshow(
+            np.transpose(vutils.make_grid(real_batch[0].to(device)[:64],
+                                          padding=2, normalize=True).cpu(), (1, 2, 0)))
+        # plt.show()
 
     '''
         CREATE MODELS and INITIALIZE TRAINING VARIABLES
