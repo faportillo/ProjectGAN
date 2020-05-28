c73c07504de9740d1eef0e14f9761f4d871c1a89
Felix <faportillo@ucdavis.edu>
2020-05-27 17:52:04 -0700

    minor fix

--- train_v2.py
+++ train_v2.py
@@ -145,17 +145,20 @@ if __name__ == '__main__':
     """
         TRAINING LOOP
     """
+    # Lists to keep track of progress
     img_list = []
     G_losses = []
     D_losses = []
     iters = 0
 
     print("Starting Training Loop...")
+    # For each epoch
     for epoch in range(num_epochs):
-        # for each batch in dataloader
+        # For each batch in the dataloader
         for i, data in enumerate(dataloader, 0):
+
             ############################
-            # (1) Update D network:
+            # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
             ###########################
             ## Train with all-real batch
             netD.zero_grad()
@@ -163,69 +166,64 @@ if __name__ == '__main__':
             real_cpu = data[0].to(device)
             b_size = real_cpu.size(0)
             label = torch.full((b_size,), real_label, device=device)
-            # Forward pass REAL batch through Discrim D(x)
-            d = netD(real_cpu).view(-1)
-            # Calculate loss for REAL Batch
-            d_loss = criterion(d, label)
-            d_loss.backward()
+            # Forward pass real batch through D
+            output = netD(real_cpu).view(-1)
+            # Calculate loss on all-real batch
+            errD_real = criterion(output, label)
+            # Calculate gradients for D in backward pass
+            errD_real.backward()
+            D_x = output.mean().item()
 
+            ## Train with all-fake batch
             # Generate batch of latent vectors
             noise = torch.randn(b_size, nz, 1, 1, device=device)
-            # Generate FAKE image batch with Gen
+            # Generate fake image batch with G
             fake = netG(noise)
             label.fill_(fake_label)
-            # Classify all fake batch with Discrim: D(G(z))
-            d_g = netD(fake.detach()).view(-1)
-            ###test_d = d_g.cpu().detach().numpy() # Test print
-            ###print(test_d) # Test print
-            # Calculate losses for real and fake batches
-            dg_loss = criterion(d_g, label)
-            # dis_loss = loss_discriminator(d, d_g, loss_type=loss_type, batch_size=b_size)
-            # print("Discriminator Loss {}: {}".format(dis_i, dis_loss))
-
-            # Calculate gradients for Discrim
-            dg_loss.backward()
-            dis_loss = d_loss + dg_loss
+            # Classify all fake batch with D
+            output = netD(fake.detach()).view(-1)
+            # Calculate D's loss on the all-fake batch
+            errD_fake = criterion(output, label)
+            # Calculate the gradients for this batch
+            errD_fake.backward()
+            D_G_z1 = output.mean().item()
+            # Add the gradients from the all-real and all-fake batches
+            errD = errD_real + errD_fake
             # Update D
             optimizerD.step()
-            D_x = d.mean()
-            D_G_z1 = d_g.mean()
 
             ############################
             # (2) Update G network: maximize log(D(G(z)))
             ###########################
             netG.zero_grad()
-            # Generate batch of latent vectors
-            # noise = torch.randn(batch_size, nz, 1, 1, device=device)
-            # label = torch.full((batch_size, 1), real_label, device=device)
-            label.fill_(real_label)
-            # fake = netG(noise)
-            d_g = netD(fake).view(-1, 1)
-            # Calculate Gen's loss based on output
-            # gen_loss = loss_generator(d_g, loss_type=loss_type, batch_size=batch_size)
-            gen_loss = criterion(d_g, label)
-            # Calcualte gradient for G
-            gen_loss.backward()
+            label.fill_(real_label)  # fake labels are real for generator cost
+            # Since we just updated D, perform another forward pass of all-fake batch through D
+            output = netD(fake).view(-1)
+            # Calculate G's loss based on this output
+            errG = criterion(output, label)
+            # Calculate gradients for G
+            errG.backward()
+            D_G_z2 = output.mean().item()
             # Update G
             optimizerG.step()
-            D_G_z2 = d_g.mean()
 
             # Output training stats
             if i % 50 == 0:
-                print('[{}/{}][{}/{}]\tLoss_D: {}\tLoss_G: {}\tD(x): {}\tD(G(z)): {}/{}'
-                      .format(epoch + 1, num_epochs, i, len(dataloader),
-                              dis_loss.item(), gen_loss.item(),
-                              D_x.item(), D_G_z1.item(), D_G_z2.item()))
-                # Save Losses for plotting later
-                G_losses.append(dis_loss.item())
-                D_losses.append(gen_loss.item())
-                # Check how the generator is doing by saving G's output on fixed_noise
-                if (iters % 500 == 0) or ((epoch == num_epochs - 1) and (i == len(dataloader) - 1)):
-                    with torch.no_grad():
-                        fake = netG(fixed_noise).detach().cpu()
-                    img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+                print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                      % (epoch, num_epochs, i, len(dataloader),
+                         errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
 
-                iters += 1
+            # Save Losses for plotting later
+            G_losses.append(errG.item())
+            D_losses.append(errD.item())
+
+            # Check how the generator is doing by saving G's output on fixed_noise
+            if (iters % 500 == 0) or ((epoch == num_epochs - 1) and (i == len(dataloader) - 1)):
+                with torch.no_grad():
+                    fake = netG(fixed_noise).detach().cpu()
+                img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+
+            iters += 1
 
         # Ssve models after training
         torch.save(netD.state_dict(), 'saved_models/discriminator_' + model_type + '_' + loss_type + '.pt')
